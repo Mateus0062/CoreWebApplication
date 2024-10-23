3a64e8d7ff4b82649c46b942780051c79d69d13e
Mateus0062 <mateusmendmattos@gmail.com>
2024-10-23 15:18:25 -0300

    Razor View Engine and Razor Syntax

---
+++ CoreWebApplication/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using CoreWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebApplication.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public static List<Customer> customers = new List<Customer>()
+        {
+            new Customer() {Id = 101, Name = "King", Amount = 12000},
+            new Customer() {Id = 102, Name = "Mateus", Amount = 13000}
+        };
+        
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Customer Management System";
+            ViewBag.CustomerCount = customers.Count();
+            ViewBag.CustomerList = customers;
+            return View();
+        }
+    }
+}
