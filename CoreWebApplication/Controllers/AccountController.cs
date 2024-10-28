15cd9849d841d6d368ea2f168db874241823de31
Mateus0062 <mateusmendmattos@gmail.com>
2024-10-28 20:06:38 -0300

    Demo: Weakly Typed Form

---
+++ CoreWebApplication/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WeaklyTypedForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LoginPost(string username, string password)
+        {
+            ViewBag.Username = username;
+            ViewBag.Password = password;
+            return View();
+        }
+    }
+}
